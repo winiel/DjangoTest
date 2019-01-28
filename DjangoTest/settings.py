875c09803bed0359250b0075099f098f0febeec0
SohnYongJin <winiel@smfcast.com>
2019-01-28 15:52:51 +0900

    init

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$ectjsqq^x+imv4+x64uq=$)cbc&v4hb$)c-_byd^fw1!vp83i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = ''
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#배포를 위해 수정
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'static'),
+
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
