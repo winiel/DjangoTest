09ba075be8e3422e819558fbe25c75884ea6bd68
SohnYongJin <winiel@smfcast.com>
2019-01-28 16:20:05 +0900

    init

--- DjangoTest/settings.py
+++ DjangoTest/settings.py
@@ -122,10 +122,10 @@ STATIC_URL = '/static/'
 
 
 #배포를 위해 수정
-STATICFILES_DIRS = (
-
-    os.path.join(BASE_DIR, 'static'),
-
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+# STATICFILES_DIRS = (
+#
+#     os.path.join(BASE_DIR, 'static'),
+#
+# )
+#
+# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
