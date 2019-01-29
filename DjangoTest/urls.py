7e943e8b423fb1be67316be71d6d6c4a872fd25b
SohnYongJin <winiel@smfcast.com>
2019-01-29 14:53:24 +0900

    view Test

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -1,13 +1,13 @@
 """DjangoTest URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to dt_view. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
+Function dt_view
+    1. Add an import:  from my_app import dt_view
+    2. Add a URL to urlpatterns:  path('', dt_view.home, name='home')
+Class-based dt_view
+    1. Add an import:  from other_app.dt_view import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from TestViews import views
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
